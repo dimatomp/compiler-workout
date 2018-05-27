8869bf0e3e91091ef26caf99ea1bbc74be99c4af
Dmitry Tomp <dmitrytomp@gmail.com>
2018-05-27 23:40:58 +0000

    Write SM interpreter

--- src/SM.ml
+++ src/SM.ml
@@ -53,29 +53,38 @@ let rec eval env ((cstack, stack, ((st, i, o) as c)) as conf) = function
     | cInst :: progRem ->
         let cCfg, cont = match (cInst, cstack) with
         | BINOP op, _                                 ->
-                let x :: y :: stack = stack in
+                let x::y::stack = stack in
                 let calcResult = Value.of_int @@ Expr.evalBinop op (Value.to_int y) (Value.to_int x) in
                 (cstack, calcResult :: stack, c), progRem
         | CONST v, _                                  -> (cstack, Value.of_int v :: stack, c), progRem
         | STRING v, _                                 -> (cstack, Value.of_string v :: stack, c), progRem
         | LD name, _                                  -> (cstack, State.eval st name :: stack, c), progRem
-        | ST name, _                                  -> let x :: stack = stack in (cstack, stack, (State.update name x st, i, o)), progRem
+        | ST name, _                                  -> let x::stack = stack in (cstack, stack, (State.update name x st, i, o)), progRem
         | STA (name, indices), _                      ->
                 let x :: stack = stack in
                 let indices, stack = split indices stack in
                 (cstack, stack, (Stmt.update st name x (rev indices), i, o)), progRem
         | LABEL name, _                               -> conf, progRem
         | JMP name, _                                 -> conf, env#labeled name
-        | CJMP ("z", name), _                         -> let x :: stack = stack in (cstack, stack, c), if Value.to_int x == 0 then env#labeled name else progRem
-        | CJMP ("nz", name), _                        -> let x :: stack = stack in (cstack, stack, c), if Value.to_int x != 0 then env#labeled name else progRem
+        | CJMP ("z", name), _                         -> let x::stack = stack in (cstack, stack, c), if Value.to_int x == 0 then env#labeled name else progRem
+        | CJMP ("nz", name), _                        -> let x::stack = stack in (cstack, stack, c), if Value.to_int x != 0 then env#labeled name else progRem
         | BEGIN (_, args, locals), _                  ->
                 let argv, stack = split (length args) stack in
                 let st = fold_right2 State.update (rev args) argv (State.enter st (args @ locals)) in
                 (cstack, stack, (st, i, o)), progRem
         | RET _, (progRem, oSt) :: cstack             -> (cstack, stack, (State.leave st oSt, i, o)), progRem
         | RET _, []                                   -> conf, []
-        | CALL (name, n, f), _ when env#is_label name -> ((progRem, st) :: cstack, stack, c), env#labeled name
-        | CALL (name, n, f), _                        -> env#builtin conf name n (not f), progRem
+        | CALL (name, n, _), _ when env#is_label name -> ((progRem, st) :: cstack, stack, c), env#labeled name
+        | CALL (name, n, f), _                        -> env#builtin conf name n f, progRem
+        | DROP, _                                     -> (cstack, tl stack, c), progRem
+        | DUP, _                                      -> (cstack, hd stack :: stack, c), progRem
+        | SWAP, _                                     -> let x::y::stack = stack in (cstack, y :: x :: stack, c), progRem
+        | TAG name, _                                 -> 
+                let x::stack = stack in 
+                let res = Value.of_int @@ match x with | Value.Sexp (tag, _) when tag == name -> 1 | _ -> 0 in
+                (cstack, res :: stack, c), progRem
+        | ENTER vars, _                               -> (cstack, stack, (State.push st State.undefined vars, i, o)), progRem
+        | LEAVE, _                                    -> (cstack, stack, (State.drop st, i, o)), progRem
         in
         eval env cCfg cont
 
@@ -142,50 +151,49 @@ let compile (defs, p) =
   | Expr.Call (name, args) -> call name args false
   in
   let rec compile_stmt l env = function
-  | Stmt.Assign (var, [], ex) -> env, true, expr ex @ [ST var]
-  | Stmt.Assign (var, idx, ex) -> env, true, concat (map expr idx) @ expr ex @ [STA (var, length idx)]
+  | Stmt.Assign (var, [], ex) -> env, false, expr ex @ [ST var]
+  | Stmt.Assign (var, idx, ex) -> env, false, concat (map expr idx) @ expr ex @ [STA (var, length idx)]
   | Stmt.Seq (e1, e2) -> 
-          let env, true, c1 = compile_stmt l env e1 in
-          let env, true, c2 = compile_stmt l env e2 in
-          env, true, c1 @ c2
-  | Stmt.Skip -> env, true, []
+          let env, flag1, c1 = compile_stmt l env e1 in
+          let env, flag2, c2 = compile_stmt l env e2 in
+          env, flag1 || flag2, c1 @ c2
+  | Stmt.Skip -> env, false, []
   | Stmt.If (cond, tbrc, fbrc) ->
           let condCode = expr cond in
           let fBranch, env = env#get_label in
           let ifEnd, env = env#get_label in
-          let env, true, trueCode = compile_stmt l env tbrc in
-          let env, true, trueCode = compile_stmt l env fbrc in
-          env, true, condCode @ [CJMP ("z", fBranch)] @ trueCode @ [JMP ifEnd; LABEL fBranch] @ trueCode @ [LABEL ifEnd]
-  | Stmt.Leave -> env, true, [LEAVE]
+          let env, flag1, trueCode = compile_stmt l env tbrc in
+          let env, flag2, trueCode = compile_stmt l env fbrc in
+          env, flag1 || flag2, condCode @ [CJMP ("z", fBranch)] @ trueCode @ [JMP ifEnd; LABEL fBranch] @ trueCode @ [LABEL ifEnd]
+  | Stmt.Leave -> env, false, [LEAVE]
   | Stmt.While (cond, body) -> 
           let wBegin, env = env#get_label in 
           let condCode = expr cond in
           let wEnd, env = env#get_label in
-          let env, true, bodyCode = compile_stmt l env body in
-          env, true, (LABEL wBegin :: condCode) @ [CJMP ("z", wEnd)] @ bodyCode @ [JMP wBegin]
-  | Stmt.Return None -> env, true, [JMP l]
+          let env, flag, bodyCode = compile_stmt l env body in
+          env, flag, (LABEL wBegin :: condCode) @ [CJMP ("z", wEnd)] @ bodyCode @ [JMP wBegin]
+  | Stmt.Return None -> env, false, [JMP l]
   | Stmt.Return (Some ex) -> env, true, expr ex @ [JMP l]
-  | Stmt.Call (name, args) -> env, true, call name args true
+  | Stmt.Call (name, args) -> env, false, call name args true
   | Stmt.Case (ex, patterns) ->
           let exCode = expr ex in
           let retLabel, env = env#get_label in
           let exitLabel, env = env#get_label in
-          let genCase (env, caseBody) (pat, body) = 
+          let genCase (env, flag, caseBody) (pat, body) = 
             let nonMatched, env = env#get_label in
-            let env, true, bodyCode = compile_stmt retLabel env body in
-            env, caseBody @ ENTER (Stmt.Pattern.vars pat) :: pattern retLabel pat @ bodyCode @ [JMP exitLabel; LABEL nonMatched; LEAVE]
+            let env, flag1, bodyCode = compile_stmt retLabel env body in
+            env, flag || flag1, caseBody @ ENTER (Stmt.Pattern.vars pat) :: pattern retLabel pat @ bodyCode @ [JMP exitLabel; LABEL nonMatched; LEAVE]
           in
-          let env, cases = fold_left genCase (env, []) patterns in
-          env, true, exCode @ cases @ [LABEL retLabel; LEAVE; JMP l; LABEL exitLabel; LEAVE]
+          let env, flag, cases = fold_left genCase (env, false, []) patterns in
+          env, flag, exCode @ cases @ [LABEL retLabel; LEAVE; JMP l; LABEL exitLabel; LEAVE]
   in
   let compile_def env (name, (args, locals, stmt)) =
     let lend, env       = env#get_label in
     let env, flag, code = compile_stmt lend env stmt in
     env,
     [LABEL name; BEGIN (name, args, locals)] @
-    code @
-    (if flag then [LABEL lend] else []) @
-    [END]
+    code @ 
+    [LABEL lend; RET (not flag)]
   in
   let env =
     object
@@ -201,4 +209,4 @@ let compile (defs, p) =
   in
   let lend, env = env#get_label in
   let _, flag, code = compile_stmt lend env p in
-  (if flag then code @ [LABEL lend] else code) @ [END] @ (List.concat def_code) 
+  code @ [LABEL lend; RET (not flag)]
